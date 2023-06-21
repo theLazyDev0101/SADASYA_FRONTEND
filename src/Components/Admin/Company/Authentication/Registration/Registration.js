a58621f323632af70953bb626d9e1f803677ff29
theLazyDev0101 <theLazyDev0101@gmail.com>
2023-06-21 14:59:41 +0530

    Registration Page Design Complete Final

--- src/Components/Admin/Company/Authentication/Registration/Registration.js
+++ src/Components/Admin/Company/Authentication/Registration/Registration.js
@@ -30,28 +30,42 @@ export default function Registration() {
                                 <div className="title">Signup</div>
                                     <div className="input-boxes">
                                         <div className="input-box">
-                                            <i className="fas fa-user"></i>
+                                            <img src={ImageName.IMAGE_NAME.userNameImage} className='input-icon' />
                                             <input type="text" placeholder="Enter Organization name" required />
                                         </div>
                                         <div className="input-box">
-                                            <i className="fas fa-envelope"></i>
+                                            <img src={ImageName.IMAGE_NAME.emailimage} className='input-icon' />
                                             <input type="text" placeholder="Enter Organization email" required />
                                         </div>
                                         <div className="input-box">
-                                            <i className="fas fa-envelope"></i>
+                                            <img src={ImageName.IMAGE_NAME.phoneImage} className='input-icon' />
                                             <input type="text" placeholder="Enter Contact No" required />
                                         </div>
                                         <div className="input-box">
-                                            <i className="fas fa-lock"></i>
+                                            <img src={ImageName.IMAGE_NAME.padlock} className='input-icon' />
                                             <input type="password" placeholder="Enter password" required />
+                                            <img src={ImageName.IMAGE_NAME.passHide} className='input-icon' />
+                                            <img src={ImageName.IMAGE_NAME.passShow} className='input-icon hide' />
                                         </div>
                                         <div className="input-box">
-                                            <i className="fas fa-lock"></i>
+                                            <img src={ImageName.IMAGE_NAME.padlock} className='input-icon' />
                                             <input type="password" placeholder="Re-Enter password" required />
+                                            <img src={ImageName.IMAGE_NAME.passHide} className='input-icon' />
+                                            <img src={ImageName.IMAGE_NAME.passShow} className='input-icon hide' />
                                         </div>
-                                        <button className="button input-box" onClick={addCompany}> Submit
-                                            {/* <input type="submit" value="Sumbit" /> */}
-                                        </button>
+                                        <div>
+                                            <button className="button input-box" onClick={addCompany}> 
+                                                <span className='button-text'>Submit</span>
+                                                <img src={ImageName.IMAGE_NAME.submitLoader} className='input-icon hide' />
+                                            </button>
+                                        </div>
+                                        <center><span className='text sign-up-text'>or use the any of the following options</span></center>
+                                        <div className='social-login'>
+                                            <img src={ImageName.IMAGE_NAME.google} className='social-login-icon' />
+                                            <img src={ImageName.IMAGE_NAME.teams} className='social-login-icon' />
+                                            <img src={ImageName.IMAGE_NAME.slack} className='social-login-icon' />
+                                        </div>
+
                                         <div className="text sign-up-text">Already have an account? <Link to ='/loginCompany' >Login now</Link></div>
                                     </div>
                             </div>
---
+++ src/services/password-toggle/index.js
@@ -0,0 +1,8 @@
+// import React, {useState} from 'react'
+// function passwordToggle(inputTextId, passwordShowId, passwordHideId) {
+//     const [inputTextId, setShowPassword] = useState(false)
+
+//     setShowPassword(!inputTextId)
+// }
+
+// export default passwordToggle
